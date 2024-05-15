d44bd9c53ece9352b23481ead112006e55b3e1bb
Umatejaibm <Umateja.Marni@ibm.com>
2024-05-16 00:11:38 +0530

    Add files via upload

---
+++ Java/Activities/Activity14.java
@@ -0,0 +1,38 @@
+package activities;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+
+public class Activity14 {
+    public static void main(String[] args) throws IOException {
+        try {
+            File file = new File("src/main/java/activities.txt");
+            boolean fStatus = file.createNewFile();
+            if(fStatus) {
+                System.out.println("File created successfully!");
+            } else {
+                System.out.println("File already exists at this path.");
+            }
+
+            //get the file Object
+            File fileUtil = FileUtils.getFile("src/main/java/activities.txt");
+            //Read file
+            System.out.println("Data in file: " + FileUtils.readFileToString(fileUtil, "UTF8"));
+
+            //Create directory
+            File destDir = new File("src/main/java/resources");
+            //Copy file to directory
+            FileUtils.copyFileToDirectory(file, destDir);
+
+            //Get file from new directory
+            File newFile = FileUtils.getFile(destDir, "activities.txt");
+            //Read data from file
+            String newFileData = FileUtils.readFileToString(newFile, "UTF8");
+            //Print it
+            System.out.println("Data in new file: " + newFileData);
+        } catch(IOException errMessage) {
+            System.out.println(errMessage);
+        }
+    }
+}
