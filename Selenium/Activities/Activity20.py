78ee75e07158584e7670d142db44fcad4d75c373
vinodjaganath <Vinodraj024@gmail.com>
2024-07-06 21:41:20 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity20.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+
+service = FirefoxService(GeckoDriverManager().install())
+
+# Start the Driver
+with webdriver.Firefox(service=service) as driver:
+    # Navigate to the URL
+    driver.get("https://v1.training-support.net/selenium/javascript-alerts")
+    # Print the title of the page
+    print("Page title is: ", driver.title)
+
+    # Find the confirm button and click it
+    driver.find_element(By.ID, "prompt").click()
+    # Switch focus to the alert
+    prompt_alert = driver.switch_to.alert
+    # Print the text in the alert
+    print(prompt_alert.text)
+    prompt_alert.send_keys("Python")
+    prompt_alert.accept()
---
+++ Selenium/Activities/Activity6.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+service = FirefoxService(GeckoDriverManager().install())
+
+# Start the Driver
+with webdriver.Firefox(service=service) as driver:
+
+    # Navigate to the URL
+    driver.get("https://v1.training-support.net/selenium/input-events")
+
+    # Print the title of the page
+    print("Page title is: ", driver.title)
+
+    # Declare actions variable
+    actions = ActionChains(driver)
+
+    # Then Perform the action in separate lines using the actions object
+    actions.send_keys("A") # Press the key of your first names initial
+    actions.key_down(Keys.CONTROL) # Press and hold the CONTROL key
+    actions.send_keys("a") # Press "a"
+    actions.send_keys("c") # Press "c"
+    actions.key_up(Keys.CONTROL) # Relase the CONTROL key
+    actions.perform() # Perform the action chain
