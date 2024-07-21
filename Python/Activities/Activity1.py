63a277044802efe8a36ee952cab033ba3f7e76e2
yogitabhosale7 <yogitabhosale7@gmail.com>
2024-07-21 16:10:46 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,8 @@
+
+name= input("what is your name:")
+age=int(input("what is your age:"))
+year= str((2024-age)-100)
+print(name +  "Is 100 years old:" + year)
+
+
+
---
+++ Python/Activities/Pandas1.py
@@ -0,0 +1,14 @@
+	
+# import pandas
+import pandas
+ 
+# Structure our data as a dictionary
+data = {
+  "Vehicle Type": ["Car", "Car", "Bike"],
+  "Manufacturer": ["Maruti", "Toyota", "Royal Enfield"],
+  "Model": ["Swift", "Corolla", "Thunderbird"]
+}
+ 
+# Create a new DataFrame using the data
+dataframe = pandas.DataFrame(data)
+ 
